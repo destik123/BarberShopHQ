dbe972f738b33c7606675e1f1a9a5f76b4d27883
Pavel Pakhtanov <destikwtf@gmail.com>
2017-12-07 14:22:10 +0500

    Making some changes (insert into)

--- app.rb
+++ app.rb
@@ -7,14 +7,39 @@ require 'sinatra/activerecord'
 set :database, "sqlite3:barbershop.db"
 
 class Client < ActiveRecord::Base
-
 end
 
 class Barber < ActiveRecord::Base
+end
 
+class Contact < ActiveRecord::Base
+end
+
+before do
+	@barbers = Barber.all
 end
 
 get '/' do
-	@barbers = Barber.all
 	erb :index
+end
+
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+	c = Client.new params[:client]
+	c.save
+	erb :visit 
+end
+
+get '/contacts' do
+	erb :contacts	
+end
+
+post '/contacts' do
+	@usermail = params['usermail']
+	@message = params['message_area']
+
+
 end
